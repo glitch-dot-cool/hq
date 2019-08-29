d295bff20512053a28b212e2eab726656e5f6913
agohorel <alex.oddlogic@gmail.com>
2019-08-29 19:03:37 -0400

    add icons & global stylesheet

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export default {
+  black: "#000",
+  nearblack: "#191919",
+  darkgrey: "#2b2b2b",
+  midgrey: "#4b4b4b",
+  lightgrey: "#c4c4c4",
+  offwhite: "#e6e6e6",
+  white: "#fff",
+  invalid: "#ba1a1a",
+  valid: "#0ca3d6"
+};

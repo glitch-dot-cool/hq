2e88b4d1a4aee42cafe8462ba465ca102f83dd65
agohorel <alex.oddlogic@gmail.com>
2019-09-01 15:38:30 -0400

    quit app when all windows closed

--- electron/main.js
+++ electron/main.js
@@ -58,3 +58,12 @@ const setupBrowserView = url => {
 };
 
 app.on("ready", createWindow);
+
+// quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+    app.quit();
+	}		
+});

c65867d167c7b37eaf1455771560df52f29e9d8d
agohorel <alex.oddlogic@gmail.com>
2019-09-02 18:20:10 -0400

    add sliding animation to sidebar
    TODO:
    -move spacer div accordingly
    -resize/move BrowserView accordingly

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 
 import colors from "../styles/colors";
@@ -14,9 +14,11 @@ const SidebarBackground = styled.div`
   background-color: ${colors.nearblack};
   box-shadow: 3px 0px 10px rgba(0, 0, 0, 0.2);
   z-index: 0;
+  transition: 0.5s ease-out all;
 `;
 
-const HeaderWrapper = styled.div`
+const HeaderWrapper = styled.a`
+  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -27,24 +29,73 @@ const HeaderLogo = styled.img`
   display: inline;
   width: 6rem;
   margin-right: 1rem;
+  transition: .5s ease-out transform;
 `;
 
 const HeaderTitle = styled.h1`
   display: inline;
   font-size: 4rem;
   color: ${colors.offwhite};
+  transition: .5s ease-out transform;
 `;
 
-const Sidebar = () => {
-  return (
-    <SidebarBackground>
-      <HeaderWrapper>
-        <HeaderLogo src={logo} />
-        <HeaderTitle>glitcHQ</HeaderTitle>
-      </HeaderWrapper>
-      <Nav></Nav>
-    </SidebarBackground>
-  );
-};
+class Sidebar extends Component {
+  state = {
+    sidebarOpen: true,
+    sidebarStyles: {
+      transform: `translateX(0)`
+    },
+    headerStyles: {
+      transform: `scale(1)`
+    },
+    logoStyles: {
+      transform: `translateX(0) scale(1)`
+    }
+  };
+
+  sidebarToggle = () => {
+    this.setState(prevState => {
+      return { sidebarOpen: !prevState.sidebarOpen };
+    });
+
+    if (this.state.sidebarOpen) {
+      this.setState({
+        sidebarStyles: {
+          transform: `translateX(-250px)`
+        },
+        headerStyles: {
+          transform: `scale(0)`
+        },
+        logoStyles: {
+          transform: `translateX(195px) scale(.7)`
+        }
+      });
+    } else {
+      this.setState({
+        sidebarStyles: {
+          transform: `translateX(0)`
+        },
+        headerStyles: {
+          transform: `scale(1)`
+        },
+        logoStyles: {
+          transform: `translateX(0) scale(1)`
+        }
+      });
+    }
+  };
+
+  render() {
+    return (
+      <SidebarBackground style={this.state.sidebarStyles}>
+        <HeaderWrapper onClick={this.sidebarToggle}>
+          <HeaderLogo src={logo} style={this.state.logoStyles} />
+          <HeaderTitle style={this.state.headerStyles}>glitcHQ</HeaderTitle>
+        </HeaderWrapper>
+        <Nav></Nav>
+      </SidebarBackground>
+    );
+  }
+}
 
 export default Sidebar;

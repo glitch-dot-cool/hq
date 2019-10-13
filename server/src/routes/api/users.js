825823cc8d621cdf107e96c0629ad649786b6a0c
agohorel <alex.oddlogic@gmail.com>
2019-10-13 15:52:10 -0400

    add register user functionality

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    register a user
@@ -19,12 +21,47 @@ router.post(
       "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req).errors;
 
     if (errors.length) {
       return res.status(400).json({ errors });
     } else {
+      const { name, email, password, createdAt } = req.body;
+
+      try {
+        // check if user exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already exists" }] });
+        }
+
+        // instantiate user object
+        user = new User({
+          name,
+          email,
+          password,
+          createdAt
+        });
+
+        // encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // save user to db
+        await user.save();
+
+        // return JWT
+        // @TODO
+        res.status(200).send("User successfully registered.");
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error.");
+      }
+
       return res.status(200).send("passed validation");
     }
   }

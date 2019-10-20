441e0ba160da8f8615f0dc14b38e903c87a1345b
agohorel <alex.oddlogic@gmail.com>
2019-10-20 16:32:44 -0400

    add api route for b2 createKey()
    server fetches generated (temporary) API key for B2, encrypts it with 256 bit AES-GCM, and sends it to the client

--- server/src/routes/api/files.js
+++ server/src/routes/api/files.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const crypto = require("crypto");
 
 const b2 = require("../../b2");
+const encryption = require("../../encryption");
 const auth = require("../../middleware/auth");
 
 // @route GET api/files
@@ -12,8 +14,25 @@ const auth = require("../../middleware/auth");
 router.get("/", auth, async (req, res) => {
   try {
     const fileList = await b2.listFiles();
-    console.log(fileList);
     res.json(fileList);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("server error");
+  }
+});
+
+// @route GET api/files/auth
+// @desc generate b2 api key, encrypt, and send to client
+// @access Private
+// @TODO display alert in try/catch error
+
+router.get("/auth", auth, async (req, res) => {
+  try {
+    const authObject = await b2.createKey(req.user.id);
+    const key = `${req.user.id}${req.user.id}`.substring(0, 32);
+    const encrypted = encryption.encrypt(key, authObject);
+
+    res.json({ encrypted });
   } catch (err) {
     console.error(err);
     res.status(500).send("server error");

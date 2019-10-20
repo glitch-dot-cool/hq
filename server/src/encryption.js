95a647823e1c84fc98e0dd079cf389c8c74f44be
agohorel <alex.oddlogic@gmail.com>
2019-10-20 16:29:56 -0400

    add encryption helper functions

---
+++ server/src/encryption.js
@@ -0,0 +1,26 @@
+const crypto = require("crypto");
+
+const encrypt = (key, data) => {
+  const iv = crypto.randomBytes(16);
+  let cipher = crypto.createCipheriv("aes-256-gcm", Buffer.from(key), iv);
+  let encrypted = cipher.update(JSON.stringify(data));
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return {
+    encryptedData: encrypted.toString("hex"),
+    iv: iv.toString("hex"),
+    authTag: cipher.getAuthTag()
+  };
+};
+
+const decrypt = (key, data) => {
+  let iv = Buffer.from(data.iv, "hex");
+  let encryptedData = Buffer.from(data.encryptedData, "hex");
+  let decipher = crypto.createDecipheriv("aes-256-gcm", Buffer.from(key), iv);
+  decipher.setAuthTag(data.authTag);
+  let decrypted = decipher.update(encryptedData);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  decrypted = JSON.parse(decrypted.toString());
+  return decrypted;
+};
+
+module.exports = { encrypt, decrypt };

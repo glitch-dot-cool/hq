ce4cd228340e94dfb4791df8abc9a803aedec4f0
agohorel <alex.oddlogic@gmail.com>
2019-10-21 15:16:55 -0400

    split off b2 library into server and client components
    rationale: the client should never be exposed to the master api key or the ability generate new keys. keys only live on or are generated by the server. other functions presupposing authentication in theory should be safe. right?

---
+++ electron/b2.js
@@ -0,0 +1,232 @@
+const axios = require("axios");
+const crypto = require("crypto");
+const path = require("path");
+const fs = require("fs");
+const { Readable, Writable } = require("stream");
+
+async function getBucketId() {
+  try {
+    const auth = await getAuth();
+
+    const res = await axios.get(
+      `${auth.apiUrl}/b2api/v2/b2_list_buckets?accountId=${auth.accountId}`,
+      {
+        headers: { Authorization: auth.token }
+      }
+    );
+
+    const bucketData = res.data.buckets[0];
+
+    const { bucketId, bucketName } = bucketData;
+
+    return {
+      id: bucketId,
+      name: bucketName
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getUploadUrl() {
+  try {
+    const auth = await getAuth();
+    const bucket = await getBucketId();
+
+    const res = await axios.post(
+      `${auth.apiUrl}/b2api/v2/b2_get_upload_url`,
+      { bucketId: bucket.id },
+      {
+        headers: { Authorization: auth.token }
+      }
+    );
+
+    const { authorizationToken, uploadUrl } = res.data;
+
+    return {
+      token: authorizationToken,
+      url: uploadUrl
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function uploadFile() {
+  const fileData =
+    "Each thing (a mirror's face, let us say) was infinite things";
+  const fileName = "aleph.txt";
+  const contentType = "text/plain";
+  const sha1 = crypto
+    .createHash("sha1")
+    .update(fileData)
+    .digest("hex");
+
+  try {
+    const uploadUrl = await getUploadUrl();
+    const res = await axios.post(uploadUrl.url, fileData, {
+      headers: {
+        Authorization: uploadUrl.token,
+        "Content-Type": contentType,
+        "Content-Length": fileData.length,
+        "X-Bz-File-name": fileName,
+        "X-Bz-Content-Sha1": sha1,
+        "X-Bz-Info-Author": "John-Doe"
+      }
+    });
+
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function downloadFile(fileName) {
+  try {
+    const auth = await getAuth();
+    const bucket = await getBucketId();
+
+    const res = await axios.get(
+      `${auth.downloadUrl}/file/${bucket.name}/${fileName}`,
+      {
+        headers: { Authorization: auth.token }
+      }
+    );
+
+    const savePath = `/downloads/${fileName}`;
+
+    let source = new Readable();
+    source._read = () => {};
+    source.push(res.data);
+    // source.push(null);
+
+    let destination = fs.createWriteStream(savePath);
+
+    source.on("end", () => {
+      console.log("File successfully downloaded.");
+    });
+
+    source.pipe(destination);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function downloadFileById(fileName) {
+  try {
+    const auth = await getAuth();
+    const fileId = await getFileId(fileName);
+
+    const res = await axios.get(
+      `${auth.apiUrl}/b2api/v2/b2_download_file_by_id?fileId=${fileId}`,
+      {
+        headers: { Authorization: auth.token }
+      }
+    );
+
+    const savePath = `/downloads/${fileName}`;
+
+    let source = new Readable();
+    source._read = () => {};
+    source.push(res.data);
+
+    let destination = fs.createWriteStream(savePath);
+
+    source.pipe(destination);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function listFiles() {
+  try {
+    const auth = await getAuth();
+    const bucket = await getBucketId();
+
+    let startFileName = null;
+    let allFiles = [];
+
+    while (true) {
+      let res = await axios.post(
+        `${auth.apiUrl}/b2api/v2/b2_list_file_names`,
+        {
+          bucketId: bucket.id,
+          startFileName
+        },
+        {
+          headers: { Authorization: auth.token }
+        }
+      );
+
+      res.data.files.forEach(file => {
+        allFiles.push(file);
+      });
+
+      startFileName = res.data.nextFileName;
+      if (res.data.nextFileName === null) break;
+    }
+
+    return allFiles;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function deleteFile(fileNameToDelete) {
+  try {
+    const auth = await getAuth();
+
+    let fileName, fileId;
+    let allFiles = await listFiles();
+
+    allFiles.forEach(file => {
+      if (file.fileName === fileNameToDelete) {
+        fileId = file.fileId;
+        fileName = file.fileName;
+      }
+    });
+
+    const res = await axios.post(
+      `${auth.apiUrl}/b2api/v2/b2_delete_file_version`,
+      {
+        fileName,
+        fileId
+      },
+      {
+        headers: { Authorization: auth.token }
+      }
+    );
+
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getFileId(fileName) {
+  try {
+    const files = await listFiles();
+    let fileId;
+
+    files.forEach(file => {
+      if (file.fileName === fileName) {
+        fileId = file.fileId;
+      }
+    });
+
+    return fileId;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = {
+  getBucketId,
+  getUploadUrl,
+  uploadFile,
+  downloadFile,
+  downloadFileById,
+  listFiles,
+  deleteFile,
+  getFileId
+};
--- server/src/routes/api/files.js
+++ server/src/routes/api/files.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
 
-const b2 = require("../../b2");
+const b2 = require("../../b2Master");
 const encryption = require("../../encryption");
 const auth = require("../../middleware/auth");
 
